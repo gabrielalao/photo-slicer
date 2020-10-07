f9aa3ef1199428d9d1478e38b0469e8faf7bc843
Gabriel Alao <gabrielsmacbookair@gabriels-air.home>
2020-10-07 18:32:21 -0400

    Fixed 500 error

--- application/config/config.php
+++ application/config/config.php
@@ -159,7 +159,6 @@ $config['composer_autoload'] = FALSE;
 |
 */
 $config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
-
 /*
 |--------------------------------------------------------------------------
 | Enable Query Strings
@@ -380,7 +379,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 86400;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
